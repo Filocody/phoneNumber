39b3a7286526119c4f8a869c3738e79936b4bb38
Ilya Fedotov <stigiec001@gmail.com>
2022-04-03 15:31:00 +0300

    first commit

---
+++ mainwindow.cpp
@@ -0,0 +1,35 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include <QRegularExpression>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->lineEdit, &QLineEdit::textEdited, [this](QString currentText)
+    {
+        QRegularExpression exp("^\\+\\d{11}$");
+        if(exp.match(currentText).hasMatch())
+            setOkLabel();
+        else setNotOkLabel();
+    });
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::setOkLabel()
+{
+    ui->label->setText("Ok!");
+    ui->label->setStyleSheet("QLabel {color : green}");
+}
+
+void MainWindow::setNotOkLabel()
+{
+    ui->label->setText("Not ok!");
+    ui->label->setStyleSheet("QLabel {color : red}");
+}
+
